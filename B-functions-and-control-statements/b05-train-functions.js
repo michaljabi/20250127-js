12d5e97d4e32c4324e38a099b8e5c21229c962e0
Jabi <michal.jabi@gmail.com>
2025-01-27 15:41:43 +0100

    task(b05): train functions.

--- B-functions-and-control-statements/b05-train-functions.js
+++ B-functions-and-control-statements/b05-train-functions.js
@@ -3,3 +3,10 @@
 // called without an argument
 // when called with the argument 'Michael' will display the text: 'Hello Michael'
 // Consider the correct names for the function and its parameter
+function sayHello(name = 'Stranger') {
+    // console.log('Hello', name)
+    console.log('Hello ' + name)
+}
+
+sayHello()
+sayHello('Michael')
