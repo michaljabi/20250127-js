ccc349550640f5db2c3c5e64869b336d361be895
Jabi <michal.jabi@gmail.com>
2025-01-28 11:29:40 +0100

    task(c04): make it work

--- C-complex-types-using-objects-and-arrays/c04-train-objects.js
+++ C-complex-types-using-objects-and-arrays/c04-train-objects.js
@@ -6,6 +6,14 @@ tax (type: number)
 
 come up with sample values yourself
 */
+const shoppingCartItem = {
+	name: 'Komputer',
+	price: 2000,
+	tax: 1.23
+}
+
+const grossPrice = shoppingCartItem.price * shoppingCartItem.tax;
+console.log(grossPrice);
 
 /* # 2 Task:
 Define and assign a shoppingCartItem2 object with the fields:
@@ -18,7 +26,30 @@ come up with sample values yourself
 Show in console.log the sums of prices (price) and taxes (tax) of both products as the value of the basket
 */
 
+const shoppingCartItem2 = {
+	name: 'Smatphone',
+	price: 3000,
+	tax: 1.23
+}
+
+const grossPrice2 = shoppingCartItem2.price * shoppingCartItem2.tax;
+console.log(grossPrice2);
+
+const cartTotalPrice = grossPrice + grossPrice2;
+console.log(grossPrice + grossPrice2);
 
 // # 3 Show that initializing two new empty objects after comparing === will give us false.
 // Even when they got the same properties and values (!!!)
 // Explain why this is happening?
+
+const obj1 = {};
+const obj2 = {};
+
+console.log(obj1 === obj2)
+
+// methematical solution to write it in one line;
+console.log({} === {});
+
+// another:
+const isEqual = {} === {};
+console.log(isEqual)
