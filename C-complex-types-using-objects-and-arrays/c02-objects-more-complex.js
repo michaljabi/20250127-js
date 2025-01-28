b895c1363491ec0eb11cf64bcdfb77c3f65bd94c
Jabi <michal.jabi@gmail.com>
2025-01-28 10:17:06 +0100

    task(c02): make it work + right.

--- C-complex-types-using-objects-and-arrays/c02-objects-more-complex.js
+++ C-complex-types-using-objects-and-arrays/c02-objects-more-complex.js
@@ -11,6 +11,10 @@ const user = {
 	address: {
 		village: 'Willoughby',
 		shireCounty: 'Lincolnshire',
+		// district: 'East Lindsey', // hardcoded version of the solution.
+		district: '', // dostrict should be here as empty string, to show us
+					  // the type of the district (string), and that this property
+					  // can exist in address.
 		house: {
 			no: 23
 		}
@@ -20,6 +24,20 @@ const user = {
 
 // #1 Task:
 // Print on console - the "no" of the house form user:
-
+console.log(user)
+console.log(user.address)
+console.log(user.address.house)
+// solution
+console.log(user.address.house.no)
 // #2 Task:
 // Add "district" "East Lindsey" to the user address
+user.address.district = "East Lindsey";
+
+console.log(user.address)
+console.log(user.address.district)
+
+// this solution will override existing adress object
+user.address = { district: 'East Lindsey' }
+
+console.log(user);
+console.log(user.address);
