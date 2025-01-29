46bf28900d36c6d0fc3442eb0ca672b4e83371ba
Jabi <michal.jabi@gmail.com>
2025-01-29 08:37:30 +0100

    taks(d02): make it work

--- D-control-statements-and-error-handlng/d02-train-loops.js
+++ D-control-statements-and-error-handlng/d02-train-loops.js
@@ -12,9 +12,17 @@
 // 2
 // 3
 // 4
+for (let x = 1; x <= 10; x++) {
+    console.log(x)
+}
 
 
 // #2 Task
 // Show a sequence of numbers 1 to 10 next to each other on the console:
 // 1 2 3 4 5 6 7 8 9 10
-
+let collector = '';
+for (let x = 1; x <= 10; x++) {
+    // collector = collector + x + ' ';
+    collector += x + ' ';
+}
+console.log(collector.trim())
