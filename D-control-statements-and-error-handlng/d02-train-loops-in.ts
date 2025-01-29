ae4a215c11f07e6c46cadea6e54d86db01c1eff0
Jabi <michal.jabi@gmail.com>
2025-01-29 08:52:55 +0100

    taks(d02): make it right + difference in TS

---
+++ D-control-statements-and-error-handlng/d02-train-loops-in.ts
@@ -0,0 +1,12 @@
+
+const collection: number[] = [];
+for (let x = 1; x <= 10; x++) {
+    collection.push(x);
+}
+console.log(collection.join(' '));
+
+
+
+
+
+export {};
--- D-control-statements-and-error-handlng/d02-train-loops.js
+++ D-control-statements-and-error-handlng/d02-train-loops.js
@@ -21,8 +21,30 @@ for (let x = 1; x <= 10; x++) {
 // Show a sequence of numbers 1 to 10 next to each other on the console:
 // 1 2 3 4 5 6 7 8 9 10
 let collector = '';
+const collection = [];
 for (let x = 1; x <= 10; x++) {
     // collector = collector + x + ' ';
     collector += x + ' ';
+    collection.push(x);
 }
 console.log(collector.trim())
+
+// DO NOT MIX THE TYPES !
+//collection.push('888');
+
+console.log(collection.join(' '));
+
+// be DRY not WET
+// DRY -> Dont Repeat Yourself
+// WET -> Write Everything Twice
+function makeMeInlineNumbers(from = 0 , to = 0) {
+    const collection = [];
+    for (let x = from; x <= to; x++) {
+        collection.push(x);
+    }
+    return collection.join(' ');
+}
+
+console.log(makeMeInlineNumbers(10, 12))
+console.log(makeMeInlineNumbers(8, 90))
+console.log(makeMeInlineNumbers(1, 5))
